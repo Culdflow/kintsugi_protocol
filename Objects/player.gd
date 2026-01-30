ba8a6b1d06ef559a5012445b63eb2fc17a76cb91
K1000 <151030382+K1000e@users.noreply.github.com>
2026-01-30 19:55:27 +0100

    Mettre à jour la scène principale et ajouter des paramètres de rendu pour la compatibilité mobile

--- Objects/player.gd
+++ Objects/player.gd
@@ -7,5 +7,25 @@ class_name Player_sc
 @export var leftArm: BodyPart = null
 @export var rightArm: BodyPart = null
 
-func _physics_process(delta: float) -> void:
+
+@export var speed := 450.0
+@export var jump_force := 950.0
+@export var gravity := 1200.0
+
+func _physics_process(delta):
+	# Gravité
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		# éviter l'accumulation
+		velocity.y = 0.0
+
+	# Déplacement horizontal
+	var direction := Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+
+	# Saut
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = -jump_force
+
 	move_and_slide()
