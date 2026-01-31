f6aeefcdc4f1e62018046523159104d170da3b4f
culdflow <fevedimitri@gmail.com>
2026-01-31 14:52:04 +0100

    ddawda

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,7 +1,14 @@
 extends Control
 
 
+func _ready() -> void:
+	$AudioStreamPlayer2.play()
 
 func _on_button_button_down() -> void:
 	$AudioStreamPlayer.play()
+	$AudioStreamPlayer2.volume_db = $AudioStreamPlayer2.volume_db - 1
 	SceneManager.change_scene("res://UI/main_anim.tscn")
+
+
+func _on_audio_stream_player_2_finished() -> void:
+	$AudioStreamPlayer2.play()
