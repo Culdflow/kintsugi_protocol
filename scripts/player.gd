911c26d1d51c0b655780f85a40c399136e793b8a
K1000 <151030382+K1000e@users.noreply.github.com>
2026-01-31 15:33:48 +0100

    add

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,7 +23,7 @@ const gravity_speed_MAX = 500
 
 
 func _physics_process(delta: float) -> void:
-	if (Input.is_action_just_pressed("up") && is_on_floor() && rightLeg && leftLeg):
+	if (Input.is_action_just_pressed("up") && is_on_floor() && (rightLeg or leftLeg)):
 		state_machine.current_state.Transitioned.emit(state_machine.current_state, "jumpingstate")
 	if (is_on_floor()):
 		gravity_speed = 0
@@ -106,7 +106,7 @@ func _die():
 		_lose_limb(rightArm)
 	if (rightLeg):
 		_lose_limb(rightLeg)
-	get_tree().create_timer(10).timeout.connect(_death_timer_timeout)
+	get_tree().create_timer(1).timeout.connect(_death_timer_timeout)
 
 func _death_timer_timeout():
-	queue_free()
+	get_tree().change_scene_to_file(get_parent().scene_file_path)
