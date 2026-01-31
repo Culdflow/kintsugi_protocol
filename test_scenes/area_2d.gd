e175a68e1d8cd84bd838c683d99b44fe23d8e879
culdflow <fevedimitri@gmail.com>
2026-01-31 14:55:34 +0100

    del

--- test_scenes/area_2d.gd
+++ test_scenes/area_2d.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	SceneManager.change_scene("res://test_scenes/test_scene_2.tscn")
+	get_tree().change_scene_to_file("res://test_scenes/test_scene_2.tscn")
